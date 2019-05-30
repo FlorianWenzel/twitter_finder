4efbae40ccaba4e48f363a9ff8fb6f48aaf3aefa
florianwenzel <github@florian-wenzel.io>
2019-05-30 19:52:49 +0200

    inital commit

---
+++ public/script.js
@@ -0,0 +1,269 @@
+let token = localStorage.getItem('token');
+let last_tweet_timestamp;
+const socket = io();
+
+
+if(!token){
+    showLogin();
+}else{
+    socket.emit('login', token);
+}
+function showLogin() {
+    Swal.mixin({
+        input: 'text',
+        showCancelButton: true
+    })
+    .queue([
+        {
+            title: 'Username',
+            text: 'please enter your username',
+            confirmButtonText: 'Next &rarr;'
+        },
+        {
+            input: 'password',
+            title: 'Password',
+            confirmButtonText: 'Login',
+            text: 'please enter your password'
+        },
+    ])
+    .then((result, username = result.value[0], password = result.value[1]) => {
+        socket.emit('login', username, password);
+    })
+}
+socket.on('login', (t) => {
+    if(t){
+        token = token;
+        localStorage.setItem('token', t);
+    }else{
+        Swal.fire({
+            type: 'error',
+            title: 'Error',
+            text: 'Wrong username or password',
+            confirmButtonText: 'Try again'
+        })
+        .then(() => {
+            showLogin();
+        })
+    }
+})
+
+$('#keys').on('click', () => {
+    socket.emit('getKeys', token)
+})
+$('#logout').on('click', () => {
+    localStorage.removeItem('token')
+    showLogin();
+})
+$('#power').on('click', () => {
+    socket.emit('stream', token)
+})
+$('#download').on('click', () => {
+    socket.emit('download', token, $('#date_select').val(), $('#type_select').val())
+})
+socket.on('getKeys', (keys) => {
+    let badges = ''
+    if(keys)
+        for(key of keys)
+            badges += '<span class="badge badge-primary">' + key + '</span> '
+    Swal.fire({
+        title: 'Keywords',
+        customClass: {
+            confirmButton: 'btn btn-success mx-1',
+            cancelButton: 'btn btn-danger mx-1'
+          },
+        buttonsStyling: false,
+        type: '',
+        html:
+            'Aktuelle Tags:<br>' + badges,
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText:
+            '<i class="fa fa-plus"></i>',
+        confirmButtonAriaLabel: 'Add tag',
+        cancelButtonText:
+            '<i class="fa fa-minus"></i>',
+        cancelButtonAriaLabel: 'Thumbs down',
+    })
+    .then((result) => {
+        console.log(result)
+        if(result.value)
+            fireAddKeyword();
+        else if (result.dismiss === "cancel")
+            fireRemoveKeyword();
+
+    })
+})
+
+socket.on('tweet', (date) => {
+    last_tweet_timestamp = date
+})
+socket.on('power_timestamp', (running, timestamp) => {
+    const prefix = running ? '<i class="fas fa-power-off" style="color: green"></i> online since ' : '<i class="fas fa-power-off" style="color: red"></i> offline since '
+    const minutes_ago = Math.ceil((Date.now() - timestamp) / (60000))
+    const hours_ago = Math.floor((Date.now() - timestamp) / (60 * 60000))
+    $('#power_timestamp').html( prefix + (hours_ago ? (hours_ago + 'h ') : '') + minutes_ago + 'm')
+})
+
+setInterval(() => {
+    if(last_tweet_timestamp){
+        const seconds_ago = Math.ceil((Date.now() - last_tweet_timestamp) / (1000)) % 60
+        const minutes_ago = Math.floor((Date.now() - last_tweet_timestamp) / (60000))
+        $('#last_tweet').html( (minutes_ago ? (minutes_ago + 'm ') : '') + seconds_ago + 's')
+    }
+}, 1000)
+
+function fireAddKeyword(){
+    Swal.fire({
+        title: 'Add Keyword',
+        confirmButtonText: '<i class="fas fa-plus"></i>',
+        input: 'text',
+        showCancelButton: true,
+        customClass: {
+            confirmButton: 'btn btn-success mx-1',
+            cancelButton: 'btn btn-primary mx-1'
+          },
+        buttonsStyling: false,
+    })
+    .then((result, key = result.value) => {
+        if(key){
+            socket.emit('addKey', token, key)
+        }
+    })
+}
+
+function fireRemoveKeyword(){
+    Swal.fire({
+        title: 'Remove Keyword',
+        confirmButtonText: '<i class="fas fa-minus"></i>',
+        input: 'text',
+        showCancelButton: true,
+        customClass: {
+            confirmButton: 'btn btn-danger mx-1',
+            cancelButton: 'btn btn-primary mx-1'
+          },
+        buttonsStyling: false,
+    })
+    .then((result, key = result.value) => {
+        if(key){
+            socket.emit('delKey', token, key)
+        }
+    })
+}
+
+socket.on('download', (type, filename, rows) => {
+    switch (type) {
+        case 'csv':
+            exportToCsv(filename, rows)
+            break;
+        case 'txt':
+            exportToTxt(filename, rows)
+            break;
+        case 'json':
+            exportToJson(filename, rows)
+            break;
+
+    }
+})
+
+function exportToCsv(filename, rows) {
+    const processRow = function (row) {
+        let finalVal = '';
+        for (let j = 0; j < row.length; j++) {
+            let innerValue = row[j] === null ? '' : row[j].toString();
+            if (row[j] instanceof Date) {
+                innerValue = row[j].toLocaleString();
+            };
+            let result = innerValue.replace(/"/g, '""');
+            if (result.search(/("|,|\n)/g) >= 0)
+                result = '"' + result + '"';
+            if (j > 0)
+                finalVal += ',';
+            finalVal += result;
+        }
+        return finalVal + '\n';
+    };
+
+    let csvFile = '';
+    for(const row of rows){
+        csvFile += processRow(row)
+    }
+
+    const blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
+
+function exportToTxt(filename, rows) {
+    const filed_names = rows.shift();
+
+    let file = ''
+    for(const row of rows){
+        for(const i in row){
+            file += filed_names[i] + ': \n\t' + row[i] + '\n'
+        }
+        file += '\n\n'
+
+    }
+
+    const blob = new Blob([file], { type: 'text/txt;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename + '.txt');
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename + '.txt');
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
+function exportToJson(filename, rows) {
+    const filed_names = rows.shift();
+
+    let file = '[\n'
+    for(const j in rows){
+        file += '\t{\n'
+        for(const i in rows[j]){
+            file += '\t\t"' + filed_names[i].toLowerCase() + '": "' + rows[j][i] + '"' + (parseInt(i) === rows[j].length - 1 ? '\n' : ',\n')
+        }
+
+        file += '\t}' + (parseInt(j) !== rows.length - 1 ? ',' : '') + '\n'
+    }
+    file += ']'
+
+    const blob = new Blob([file], { type: 'text/json;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename + '.json');
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename + '.json');
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
---
+++ server.js
@@ -0,0 +1,160 @@
+"use strict";
+const express = require('express')
+const path = require('path')
+const bcrypt = require('bcrypt')
+const MongoClient = require('mongodb').MongoClient
+const Twit = require('twit')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const chalk = require('chalk')
+const credentials = require('./credentials.js')
+
+let db, users, stream, tweets, last_tweet_timestamp, power_timestamp = Date.now()
+const Twitter = new Twit(credentials);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+const PORT = 7777
+http.listen(PORT, function () {
+    console.log(chalk.green('App started on port ' + PORT))
+})
+
+const db_url = 'mongodb://' + process.env.DB_USR + ':' +  process.env.DB_PW + '@localhost:27017?authMechanism=DEFAULT&authSource=hyrule'
+MongoClient.connect(db_url, { useNewUrlParser: true }, async function(err, client) {
+    console.log(chalk.green('connected to mongodb'))
+
+    db = await client.db('hyrule')
+    users = db.collection('users')
+    tweets = db.collection('tweets')
+})
+
+io.on('connection', (socket) => {
+    socket.on('login', async (username, password) => {
+        const token = !password ? username : undefined
+        let valid = false
+        if(token){
+            valid = !!(await users.findOne({token: token}))
+        }else{
+            const hash = (await users.findOne({username: username})).password
+            valid =  !hash ? false : await bcrypt.compare(password, hash)
+        }
+
+        if(valid){
+            socket.join('loggedIn')
+            socket.emit('power_timestamp', !!stream, power_timestamp)
+            socket.emit('tweet', last_tweet_timestamp)
+            socket.emit('login', (await users.findOne({$or: [{username: username}, {token: token}]})).token)
+        }else{
+            socket.emit('login', false)
+        }
+    })
+    socket.on('getKeys', async (token) => {
+        const user = await users.findOne({token: token});
+        if(!user){
+            socket.emit('login', false);
+            return;
+        }
+        socket.emit('getKeys', user.keys);
+    })
+    socket.on('addKey', async (token, key) => {
+        const user = await users.findOne({token: token});
+        if(!user){
+            socket.emit('login', false);
+            return;
+        }
+        const keys = (user.keys ? user.keys : []);
+        keys.push(key);
+        await users.updateOne({token: token}, {$set: {keys: keys}})
+        socket.emit('getKeys', keys);
+    })
+    socket.on('delKey', async (token, key) => {
+        const user = await users.findOne({token: token});
+        if(!user){
+            socket.emit('login', false);
+            return;
+        }
+        let keys = (user.keys ? user.keys : []);
+        const i = keys.indexOf(key);
+        if(i > -1)
+            keys.splice(i, 1)
+        await users.updateOne({token: token}, {$set: {keys: keys}})
+        socket.emit('getKeys', keys);
+    })
+    socket.on('download', async (token, time, format) => {
+        const user = await users.findOne({token: token});
+        if(!user){
+            socket.emit('login', false);
+            return;
+        }
+        let relevant_data
+        if(time === '10' || time === '100')
+            relevant_data = await tweets.find({}).sort({$natural:1}).limit(parseInt(time)).toArray()
+
+        if(time === '24h')
+            relevant_data = await tweets.find({timestamp: {$gt: Date.now() - 24 * 60 * 60 * 1000}}).toArray()
+        if(time === '7d')
+            relevant_data = await tweets.find({timestamp: {$gt: Date.now() - 7 * 24 * 60 * 60 * 1000}}).toArray()
+        if(time === '30d')
+            relevant_data = await tweets.find({timestamp: {$gt: Date.now() - 30 * 24 * 60 * 60 * 1000}}).toArray()
+        if(time === 'all')
+            relevant_data = await tweets.find().toArray()
+
+        let rows = [['Date', 'Name', 'Profile', 'Tweet']]
+        for(let tweet of relevant_data){
+            rows.push([timeConverter(tweet.timestamp), tweet.user.name, 'https://twitter.com/' + tweet.user.screen_name, 'https://twitter.com/statuses/' + tweet.id_str])
+        }
+        socket.emit('download', format, 'file', rows)
+    })
+    socket.on('stream', token => toggleStream(token, socket))
+})
+
+async function toggleStream(token, socket){
+    const user = await users.findOne({token: token})
+    power_timestamp = Date.now()
+    if(!user){
+        socket.emit('login', false)
+        return;
+    }
+    //if already streaming stop streaming
+    if(stream){
+        stream.stop()
+        console.log(chalk.red('tweet stream stopeed'))
+        stream = null
+        socket.emit('stream', false)
+        io.to('loggedIn').emit('power_timestamp', false, power_timestamp)
+        return
+    }
+    stream = Twitter.stream('statuses/filter', { track: user.keys });
+    stream.on('tweet', async (tweet) => {
+        last_tweet_timestamp = Date.now()
+        io.to('loggedIn').emit('tweet', last_tweet_timestamp)
+        //check if tweeter is already in database
+        const known_tweeter = !!(await tweets.findOne({"user.id": tweet.user.id}))
+        if(known_tweeter)
+            return
+        tweets.insertOne({...tweet, timestamp: Date.now()})
+    });
+    stream.on('connected', () => {
+        console.log(chalk.green('tweet stream started'))
+        console.log(power_timestamp)
+        io.to('loggedIn').emit('power_timestamp', true, power_timestamp)
+    })
+    await users.updateOne(user, {$set: {streaming: Date.now()}});
+}
+
+function timeConverter(UNIX_timestamp){
+    const a = new Date(UNIX_timestamp * 1000);
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const hour = a.getHours();
+    const min = a.getMinutes();
+    const sec = a.getSeconds();
+    return date + ' ' + month + ' ' + (hour > 9 ? hour : '0' + hour) + ':' + (min > 9 ? min : '0' + min);
+}
